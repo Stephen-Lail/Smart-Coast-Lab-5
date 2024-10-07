6dd1ac3076c4a38600033a57494a946608f01947
Stephen-Lail <sml6042@uncw.edu>
2024-10-07 15:29:20 -0400

    add Transmitting code

---
+++ Lab-5-Transmitting/src/Lab-5-Transmitting.cpp
@@ -0,0 +1,47 @@
+/* 
+ * Project: Smart Coast lab 5
+ * Author: Stephen Lail
+ * Date: 10/7/2024
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+// Run the application and system concurrently in separate threads
+SYSTEM_THREAD(ENABLED);
+
+const int myPin = 9;
+
+// Show system, cloud connectivity, and application logs over USB
+// View logs with CLI using 'particle serial monitor --follow'
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+// setup() runs once, when the device is first turned on
+void setup() {
+  // Put initialization like pinMode and begin functions here
+  Serial.begin(9600);
+  pinMode(myPin,OUTPUT);
+
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  
+  digitalWrite(myPin, HIGH);
+  delay(500);
+  digitalWrite(myPin,LOW);
+  delay(2000);
+
+  // The core of your code will likely live here.
+
+  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
+  // Log.info("Sending Hello World to the cloud!");
+  // Particle.publish("Hello world!");
+  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+}
